3943a54f5553fe027e61b5bc0be3bb9d77cafed3
Haseeb Ahmad <43096403+hasebsiddiqui@users.noreply.github.com>
2022-06-14 03:03:28 +0500

    REACT To V17

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ function App() {
             <Route exact path="/mainatc" element={<MainATC />} />
             <Route exact path="/appatc" element={<AppATC />} />
             <Route exact path="/headeratc" element={<HeaderATC />} />
+
             <Route exact path="/test" element={<Test />} />
             <Route path="/" element={<NotFound />} />
           </Routes>
